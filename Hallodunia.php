7cce3efb2bdbfdc4b57a5fce540d5623fbf25cc9
herlin13 <92774366+herlin13@users.noreply.github.com>
2021-10-19 20:02:29 +0700

    Update Hallodunia.php
    t-shirt plis

--- Hallodunia.php
+++ Hallodunia.php
@@ -5,5 +5,5 @@
 // ini juga komentar satu baris tapi dengan tanda double-slash (//)
 
 echo "Halo dunia!";
-echo "Hacktoberfest-indonesia-2021";
-echo "Freedom13";
+echo "Hacktoberfest-indonesia";
+echo "Hacktober-2021";
